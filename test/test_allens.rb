6fee3b617a97bfecd00f5309a873d3da586748d5
Andrew Clarke <a.andrew.clarke@gmail.com>
2016-03-29 12:53:11 +1100

    Filled out the tests to cover both fundamental and higher order operators, for interval-interval, point-interval and point-point combinations, including valid cases where the interval spans to 'forever'.

--- test/test_allens.rb
+++ test/test_allens.rb
@@ -30,65 +30,143 @@ class AllensTest < Test::Unit::TestCase
   # designed to test Intervals that are both limited and have a 'forever' ends value.
   # The list of numbers shows which of the higher operators should be true for
   # this combo; these extra lists are ignored for testing the fundamental operators.
-  #
-  VALS = [ [ "xx...", "...xx", [ 0, 0, 0, 0, 1 ] ], [ "xx...", "...XX", [ 0, 0, 0, 0, 1 ] ],
-           [ "xx...", "..xxx", [ 0, 0, 0, 0, 1 ] ], [ "xx...", "..XXX", [ 0, 0, 0, 0, 1 ] ],
-           [ "xxx..", "..xxx", [ 0, 0, 0, 1, 0 ] ], [ "xxx..", "..XXX", [ 0, 0, 0, 1, 0 ] ],
-           [ "xx...", "xxxxx", [ 1, 1, 1, 1, 0 ] ], [ "xx...", "XXXXX", [ 1, 1, 1, 1, 0 ] ],
-           [ ".xxx.", "xxxxx", [ 0, 1, 1, 1, 0 ] ], [ ".xxx.", "XXXXX", [ 0, 1, 1, 1, 0 ] ],
-           [ "..xxx", "xxxxx", [ 1, 1, 1, 1, 0 ] ], [ "..XXX", "XXXXX", [ 1, 1, 1, 1, 0 ] ],
-           [ "xxxxx", "xxxxx", [ 0, 0, 1, 1, 0 ] ], [ "XXXXX", "XXXXX", [ 0, 0, 1, 1, 0 ] ],
-           [ "xxxxx", "..xxx", [ 1, 1, 1, 1, 0 ] ], [ "XXXXX", "..XXX", [ 1, 1, 1, 1, 0 ] ],
-           [ "xxxxx", ".xxx.", [ 0, 1, 1, 1, 0 ] ], [ "XXXXX", ".xxx.", [ 0, 1, 1, 1, 0 ] ],
-           [ "xxxxx", "xx...", [ 1, 1, 1, 1, 0 ] ], [ "XXXXX", "xx...", [ 1, 1, 1, 1, 0 ] ],
-           [ "..xxx", "xxx..", [ 0, 0, 0, 1, 0 ] ], [ "..XXX", "xxx..", [ 0, 0, 0, 1, 0 ] ],
-           [ "..xxx", "xx...", [ 0, 0, 0, 0, 1 ] ], [ "..XXX", "xx...", [ 0, 0, 0, 0, 1 ] ],
-           [ "...xx", "xx...", [ 0, 0, 0, 0, 1 ] ], [ "...XX", "xx...", [ 0, 0, 0, 0, 1 ] ] ]
+  INTERVALS = [
+    [ "xx...", "...xx", [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ], [ "xx...", "...XX", [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ],
+    [ "xx...", "..xxx", [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ], [ "xx...", "..XXX", [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ],
+    [ "xxx..", "..xxx", [ 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0 ] ], [ "xxx..", "..XXX", [ 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0 ] ],
+    [ "xx...", "xxxxx", [ 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0 ] ], [ "xx...", "XXXXX", [ 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0 ] ],
+    [ ".xxx.", "xxxxx", [ 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0 ] ], [ ".xxx.", "XXXXX", [ 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0 ] ],
+    [ "..xxx", "xxxxx", [ 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0 ] ], [ "..XXX", "XXXXX", [ 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0 ] ],
+    [ "xxxxx", "xxxxx", [ 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0 ] ], [ "XXXXX", "XXXXX", [ 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0 ] ],
+    [ "xxxxx", "..xxx", [ 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0 ] ], [ "XXXXX", "..XXX", [ 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0 ] ],
+    [ "xxxxx", ".xxx.", [ 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0 ] ], [ "XXXXX", ".xxx.", [ 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0 ] ],
+    [ "xxxxx", "xx...", [ 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0 ] ], [ "XXXXX", "xx...", [ 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0 ] ],
+    [ "..xxx", "xxx..", [ 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0 ] ], [ "..XXX", "xxx..", [ 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0 ] ],
+    [ "..xxx", "xx...", [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ], [ "..XXX", "xx...", [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ],
+    [ "...xx", "xx...", [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ], [ "...XX", "xx...", [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ]
+  ]
+
+  # "Now let's consider the special case in which one of the two time periods is a point in time,
+  #  i.e. is exactly one clock tick in length, and the other one contains two or more clock ticks.
+  #  This point in time may either [intersects] or [excludes] the time period.
+  #  If the point in time [intersects] the time period, it also [fills] and [occupies] that time period.
+  #  If it [aligns] with the time period, then it either [starts] the time period or [finishes] it.
+  #  Otherwise, the point in time occurs [during] the time period.
+  #  If the point in time [excludes] the time period, then either may be [before] the other, or they may [meet]."
+  POINT_INTERVAL = [
+    [ "x....", "..xxx", [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ], [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ],
+    [ "x....", "..XXX", [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ], [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ],
+    [ "x....", ".xxxx", [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ], [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ],
+    [ "x....", ".XXXX", [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ], [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ],
+    [ "x....", "xxxxx", [ 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 ], [ 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0 ] ],
+    [ "x....", "XXXXX", [ 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 ], [ 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0 ] ],
+    [ "..x..", "xxxxx", [ 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 ], [ 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0 ] ],
+    [ "..x..", "XXXXX", [ 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 ], [ 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0 ] ],
+    [ "....x", "xxxxx", [ 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 ], [ 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0 ] ],
+    [ "....x", "XXXXX", [ 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 ], [ 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0 ] ],
+    [ "....x", "xxxx.", [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 ], [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ],
+    [ "....x", "xxx..", [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ], [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ]
+  ]
+
+  # "Finally, let's consider one more special case, that in which both the time periods are points in time.
+  #  Those two points in time may be [equal], or one may be [before] the other, or they may [meet].
+  #  There are no other Allen relationships possible for them."
+  POINTS = [
+    [ "x..", "..x", [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ], [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ],
+    [ ".x.", "..x", [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ], [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ],
+    [ ".x.", ".x.", [ 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 ], [ 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0 ] ],
+    [ "..x", ".x.", [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 ], [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ],
+    [ "..x", "x..", [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ], [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ] ]
+  ]
 
   # TODO: checks for class constraints triggering exceptions
   # eg not defining chronon, clocktick, forever
   # also need tests for insensible (ie out of bounds) end values throwing exceptions.
 
 
-  def test_fundamental_operators
-    i = 0
-    k = 0
-    VALS.each do |(sX, sY, _)|
-      # step i by 1 every two pairs; will run as: 1, 1, 2, 2, ..., 13, 13 (13 tests - are you paying attention?)
-      k = 1 - k
-      i += k
-
+  def test_intervals_fundamental_operators
+    i, step = -1, 0
+    INTERVALS.each do |(sX, sY, _)|
+      i += step = 1 - step
       pX = TestInterval.maker(sX)
       pY = TestInterval.maker(sY)
 
-      j = 0
-      FUNDAMENTAL_TESTS.each do |test|
-        j += 1
+      FUNDAMENTAL_TESTS.each_with_index do |test, j|
         want = i == j
         assert_equal want, pX.send(test, pY)
       end
     end
   end
+  # test_intervals_fundamental_operators
 
 
-  def test_higher_operators
-    i = 0
-    k = 0
-    VALS.each do |(sX, sY, expects)|
-      # step i by 1 every two pairs; will run as: 1, 1, 2, 2, ..., 13, 13 (13 tests - are you paying attention?)
-      k = 1 - k
-      i += k
-
+  def test_intervals_higher_operators
+    INTERVALS.each do |(sX, sY, expects)|
       pX = TestInterval.maker(sX)
       pY = TestInterval.maker(sY)
 
-      j = 0
-      HIGHER_TESTS.each do |test|
-        want = expects[j] == 1
-        j += 1
+      HIGHER_TESTS.each_with_index do |test, i|
+        want = expects[i] == 1
         assert_equal want, pX.send(test, pY)
       end
     end
   end
+  # test_intervals_higher_operators
+
+
+  def test_point_interval_fundamental_operators
+    POINT_INTERVAL.each do |(sX, sY, expects, _)|
+      pX = TestInterval.maker(sX)
+      pY = TestInterval.maker(sY)
+
+      FUNDAMENTAL_TESTS.each_with_index do |test, i|
+        want = expects[i] == 1
+        assert_equal want, pX.send(test, pY)
+      end
+    end
+  end
+  # test_point_interval_fundamental_operators
+
+
+  def test_point_interval_higher_operators
+    POINT_INTERVAL.each do |(sX, sY, _, expects)|
+      pX = TestInterval.maker(sX)
+      pY = TestInterval.maker(sY)
+
+      HIGHER_TESTS.each_with_index do |test, i|
+        want = expects[i] == 1
+        assert_equal want, pX.send(test, pY)
+      end
+    end
+  end
+  # test_point_interval_higher_operators
+
+
+  def test_points_fundamental_operators
+    POINTS.each do |(sX, sY, expects, _)|
+      pX = TestInterval.maker(sX)
+      pY = TestInterval.maker(sY)
+
+      FUNDAMENTAL_TESTS.each_with_index do |test, i|
+        want = expects[i] == 1
+        assert_equal want, pX.send(test, pY)
+      end
+    end
+  end
+  # test_points_fundamental_operators
+
+
+  def test_points_higher_operators
+    POINTS.each do |(sX, sY, _, expects)|
+      pX = TestInterval.maker(sX)
+      pY = TestInterval.maker(sY)
+
+      HIGHER_TESTS.each_with_index do |test, i|
+        want = expects[i] == 1
+        assert_equal want, pX.send(test, pY)
+      end
+    end
+  end
+  # test_points_higher_operators
 end
 
